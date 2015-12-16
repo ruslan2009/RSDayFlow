8aaeb17c1dc86859c88d0c983c6c474e6603dbc2
Ruslan <ruslan@Ruslans-MacBook-Pro.local>
2015-12-16 16:42:56 +0200

    added configuration option to display start/end months as partial

--- RSDayFlow/RSDFDatePickerView.m
+++ RSDayFlow/RSDFDatePickerView.m
@@ -489,6 +489,9 @@ static NSString * const RSDFDatePickerViewDayCellIdentifier = @"RSDFDatePickerVi
 {
     NSInteger monthSection = [self sectionForDate:date];
     NSDate *firstDayInMonth = [self dateForFirstDayInSection:monthSection];
+    if (self.partialMonthsEnabled && self.startDate) {
+        firstDayInMonth = [firstDayInMonth laterDate:self.startDate];
+    }
     NSUInteger weekday = [self reorderedWeekday:[self.calendar components:NSCalendarUnitWeekday fromDate:firstDayInMonth].weekday];
     NSInteger dateItem = [self.calendar components:NSCalendarUnitDay fromDate:firstDayInMonth toDate:date options:0].day + weekday;
     NSIndexPath *indexPath = [NSIndexPath indexPathForItem:dateItem inSection:monthSection];
